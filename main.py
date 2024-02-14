7cb4d2f539ad8810c76edc50385aad102651054a
utsav <sav.acharya22@gmail.com>
2024-02-14 20:03:10 +1100

    blueprint

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
 
+import os
+from flask import Flask
+from init import db,ma,bcrypt,jwt 
 
-db = SQLAlchemy()
-ma = Marshmallow()
-bcrypt = Bcrypt()
-jwt = JWTManager()
+def create_app():
+    app = Flask(__name__)
+
+    # configs
+    app.config["SQLALCHEMY_DATABASE_URI"] =os.environ.get("DATABASE_URI")
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    # connect libraries with flask app
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    from controllers.cli_controller import db_commands
+    app.register_blueprint(db_commands)
+
+    from controllers.auth_controller import auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
